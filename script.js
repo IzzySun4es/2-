5893d706259b056919a91423dff59a32e33d5636
Alexsandra Chugaenko <sascha281201@gmail.com>
2023-04-14 21:00:35 +0300

    ва

--- script.js
+++ script.js
@@ -6,6 +6,20 @@ function toggleMenu(){
     menu.classList.toggle('open');
 }
 
+window.onscroll = function() {myFunction()};
+
+var nav = document.getElementById("nav");
+var sticky = nav.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    nav.classList.add("sticky")
+  } else {
+    nav.classList.remove("sticky");
+  }
+}
+
+
 filterSelection("all")
 function filterSelection(c) {
   var x, i;
